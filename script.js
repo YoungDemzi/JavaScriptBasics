eb7b30f7812ea1cbdc43357e0233a8bd1cb0c6b5
YoungDemzi <sebastian.demzala@gmail.com>
2023-02-01 03:52:14 +0100

    Table exercises

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-const textBox = document.querySelector("#textBox");
-const output = document.querySelector("#output");
 
-textBox.addEventListener('keydown', (event) => output.textContent = `You pressed "${event.key}".`);
+
